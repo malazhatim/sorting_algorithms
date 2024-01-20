ed67ffeb03f15f4d62eb084ab7b0fa85b430df5b
Esra-333 <esra.omer444@gmail.com>
2024-01-19 23:48:02 -0800

    a

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
